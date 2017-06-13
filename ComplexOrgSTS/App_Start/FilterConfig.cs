9cff3e49b45142072083f8976289b50bcc19ac5c
Brett Hacker <brhacker@microsoft.com>
2017-06-13 16:19:48 -0700

    Remove appinsights from custom STS

--- ComplexOrgSTS/App_Start/FilterConfig.cs
+++ ComplexOrgSTS/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace ComplexOrgSTS
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute());
         }
     }
 }
