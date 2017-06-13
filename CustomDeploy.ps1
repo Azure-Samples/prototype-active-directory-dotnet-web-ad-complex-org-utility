ba9baa7ecee33fc31c97d1161b93bcbb10f65f6c
Brett Hacker <brhacker@microsoft.com>
2017-06-13 15:38:47 -0700

    ARM debug, fix dependency path in OrgRelay project file

--- CustomDeploy.ps1
+++ CustomDeploy.ps1
@@ -3,92 +3,89 @@ Import-Module Azure -ErrorAction SilentlyContinue
 
 #DEPLOYMENT OPTIONS
     #optional, defines uniqueness for deployments
-    $TestNo                  = "4"
-    #region to deploy into - see https://azure.microsoft.com/en-us/regions/
-    $DeployRegion            = "West US 2"
-    #Name of your company - will be displayed through your site
-    $CompanyName             = "Contoso"
-
-    #The name of the Azure AD tenant that will host this app
-    $TenantName              = "contoso.com"
-    #The GUID of that tenant
-    $AADTenantId             = "[AAD TenantID for auth app hosting]"
-    #The name of your Azure subscription associated with your Azure AD auth tenant
-    $AADSubName              = "ADTestTenant"
-
-    #The GUID of your Azure AD tenant that's associated with your Azure subscription (where the site will be deployed)
-    $AzureTenantId           = "[AAD TenantID for web app hosting]"
-    #The name of that subscription
-    $AzureSubName            = "MyAzureSubscription"
+    $TestNo                  = "1"
 
     #The name of the Resource Group where all of these resources will be deployed
     $RGName                  = "ComplexOrgPOC$TestNo"
+
     #The "name" of your web application
     $SiteName                = "ComplexOrgAdmin$TestNo"
 
+    $CustomSTSDisplayName    = "Complex Org Login"
+
     #The display name of your Azure AD administrative auth app. This name is displayed when a user logs in to your app from Azure AD
     $AdminAppName            = "Complex Org Administration$TestNo"
-    #A unique URI that defines your application
-    $AdminAppUri             = "https://$($SiteName).$TenantName"
-    
-    #generating a unique "secret" for your admin app to execute B2B operations on your behalf
+
+    #region to deploy into - see https://azure.microsoft.com/en-us/regions/
+    $DeployRegion            = "West US 2"
+
+    #The name of the Azure AD tenant that will host this app
+    $TenantName              = "[your AAD Tenant name, like contoso.com or contoso.onmicrosoft.com]"
+
+    #The GUID of that tenant
+    $AADTenantId             = "[AAD TenantID for app hosting]"
+
+    #Azure AD Application Client ID
+    $ClientId                = $null
+
+    #App secret for that app
+    $ClientSecret            = $null
+#END DEPLOYMENT OPTIONS
+
+#Dot-sourced variable override (optional, comment out if not using)
+. C:\dev\A_CustomDeploySettings\ComplexOrgSettings.ps1
+
+$newApps = $false;
+
+if ($ClientId -eq $null) {
+    #generating a unique "secret" for your admin app to execute operations on your behalf
 	$bytes = New-Object Byte[] 32
 	$rand = [System.Security.Cryptography.RandomNumberGenerator]::Create()
 	$rand.GetBytes($bytes)
 	$rand.Dispose()
 	$spAdminPassword = [System.Convert]::ToBase64String($bytes)
 
-#END DEPLOYMENT OPTIONS
+    #A unique URI that defines your application
+    $AdminAppUri             = "https://$($SiteName).$TenantName"
 
-#Dot-sourced variable override (optional, comment out if not using)
-. C:\dev\A_CustomDeploySettings\ComplexOrgSettings.ps1
+    $adminApp = Get-AzureRmADApplication -DisplayNameStartWith $AdminAppName -ErrorAction Stop
+    if ($adminApp -eq $null) {
+        #generate required AzureAD applications
+        #note: setting loopback on apps for now - will update after the ARM deployment is complete (below)...
+        $adminApp = New-AzureRmADApplication -DisplayName $AdminAppName -HomePage "https://loopback" -IdentifierUris $AdminAppUri
+            New-AzureRmADServicePrincipal -ApplicationId $adminApp.ApplicationId
+        $newApps = $true
+    }
 
-#ensure we're logged in
-try {
-    $ctx=Get-AzureRmContext -ErrorAction Stop
-}
-catch {
-    Login-AzureRmAccount -SubscriptionName $AADSubName -TenantId $AADTenantId -ErrorAction Stop
-}
+    if ($newApps) {
+        Start-Sleep 15
+    }
 
-#this will only work if the same account can see the tenant and Azure sub at the same time
-Set-AzureRmContext -TenantId $AADTenantId -SubscriptionName $AADSubName -ErrorAction Stop
-
-$newApps = $false;
-
-$adminApp = Get-AzureRmADApplication -DisplayNameStartWith $AdminAppName -ErrorAction Stop
-if ($adminApp -eq $null) {
-    #generate required AzureAD applications
-    #note: setting loopback on apps for now - will update after the ARM deployment is complete (below)...
-    $adminApp = New-AzureRmADApplication -DisplayName $AdminAppName -HomePage "https://loopback" -IdentifierUris $AdminAppUri
-        New-AzureRmADServicePrincipal -ApplicationId $adminApp.ApplicationId
-    $newApps = $true
-}
-
-if ($newApps) {
-    Start-Sleep 15
-}
-
-$adminAppCred = Get-AzureRmADAppCredential -ApplicationId $adminApp.ApplicationId
-if ($adminAppCred -eq $null) {
-    New-AzureRmADAppCredential -ApplicationId $adminApp.ApplicationId -Password $spAdminPassword
+    $adminAppCred = Get-AzureRmADAppCredential -ApplicationId $adminApp.ApplicationId
+    if ($adminAppCred -eq $null) {
+        New-AzureRmADAppCredential -ApplicationId $adminApp.ApplicationId -Password $spAdminPassword
+    }
+    $ClientId = $adminApp.ApplicationId
+    $ClientSecret = $spAdminPassword
 }
 
 #deploy
-Set-AzureRmContext -SubscriptionName $AzureSubName -TenantId $AzureTenantId -ErrorAction Stop
-
 $parms=@{
     "hostingPlanName"             = $SiteName;
     "skuName"                     = "F1";
     "skuCapacity"                 = 1;
+    "CustomSTSDisplayName"        = $CustomSTSDisplayName;
     "tenantName"                  = $TenantName;
     "tenantId"                    = $AADTenantId;
-    "clientId_admin"              = $adminApp.ApplicationId;
-    "clientSecret_admin"          = $spAdminPassword;
+    "clientId"                    = $ClientId;
+    "clientSecret"                = $ClientSecret;
+    "AdminRedisSKUName"           = "Basic";
+    "AdminRedisSKUFamily"         = "C";
+    "AdminRedisSKUCapacity"       = 0;
 }
 
-$TemplateFile = "https://raw.githubusercontent.com/Azure-Samples/active-directory-dotnet-web-ad-complex-org-utility/master/azuredeploy.json"
-#$TemplateFile = "C:\Dev\active-directory-dotnet-web-ad-complex-org-utility\azuredeploy.json"
+#$TemplateFile = "https://raw.githubusercontent.com/Azure-Samples/active-directory-dotnet-web-ad-complex-org-utility/master/azuredeploy.json"
+$TemplateFile = "C:\Dev\ComplexOrg\active-directory-dotnet-web-ad-complex-org-utility\azuredeploy.json"
 
 try {
     Get-AzureRmResourceGroup -Name $RGName -ErrorAction Stop
@@ -99,26 +96,35 @@ catch {
     Write-Host "Created new resource group $RGName."
 }
 $version ++
-$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateParameterObject $parms -TemplateFile $TemplateFile -Name "B2BDeploy$version"  -Force -Verbose
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateParameterObject $parms -TemplateFile $TemplateFile -Name "ComplexOrgDeploy$version"  -Force -Verbose
 
 if ($deployment) {
     #to-do: update URIs and reply URLs for apps, based on output parms from $deployment
     #also to-do: update application permissions and APIs - may need to be done in the portal
-    $hostName = $Deployment.Outputs.webSiteObject.Value.enabledHostNames.Item(0).ToString()
-    $url = "https://$hostname/"
-    $adminApp.ReplyUrls.Add($url)
-    $preauthApp.ReplyUrls.Add($url)
-    #todo: update app reply urls
+    $adminHostName = $Deployment.Outputs.AdminSiteObject.Value.enabledHostNames.Item(0).ToString()
+    if ($newApps) {
+        $url = "https://$adminHostName/"
+        $adminApp.ReplyUrls.Add($url)
+        #todo: update app reply urls
+    }
+    $stsHostName = $Deployment.Outputs.StsSiteObject.Value.enabledHostNames.Item(0).ToString()
 
     $ProjectFolder = "$env:USERPROFILE\desktop\$RGName\"
     if (!(Test-Path -Path $ProjectFolder)) {
         md $ProjectFolder
     }
     $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$($ProjectFolder)Complex Org Admin Site.lnk")
-    $Shortcut.TargetPath = 
+    $Shortcut = $WshShell.CreateShortcut("$($ProjectFolder)Complex Org Admin.lnk")
+    $Shortcut.TargetPath = $adminHostName
     $Shortcut.IconLocation = "%ProgramFiles%\Internet Explorer\iexplore.exe, 0"
     $Shortcut.Save()
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$($ProjectFolder)Complex Org STS.lnk")
+    $Shortcut.TargetPath = $stsHostName
+    $Shortcut.IconLocation = "%ProgramFiles%\Internet Explorer\iexplore.exe, 0"
+    $Shortcut.Save()
+
     start $ProjectFolder
 }
 
